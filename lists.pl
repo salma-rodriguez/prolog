c150b5bece80dfca5b9723d53116458e20f26965
Salma Rodriguez <srodr063@fiu.edu>
2014-02-09 17:50:45 -0500

    Update lists.pl

--- lists.pl
+++ lists.pl
@@ -1,13 +1,13 @@
 append([],List,List).                      % append [] to list = list
 
 append([H|Tail],X,[H|NewTail]) :-
-	append(Tail,X,NewTail).                  % if we can append Tail to X
+	append(Tail,X,NewTail).            % if we can append Tail to X
 					   % then we have NewTail
 
 reverse([],[]).                            % reverse of empty list is empty list
 
 reverse([H|T],R) :-
-	reverse(T,T_reversed),                   % reverse the tail
+	reverse(T,T_reversed),             % reverse the tail
         append(T_reversed,[H],R).          % append the result to the list [H]
 
 palindrome(List) :- reverse(List, List).
